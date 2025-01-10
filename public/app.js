54ca4c7069444d437ed1155ef56cc990e6b7550b
ogabek <ogbkiy@gmail.com>
2025-01-10 15:13:49 +0500

    cvr

--- public/app.js
+++ public/app.js
@@ -1,8 +1,22 @@
 const tg = window.Telegram.WebApp;
 
-document.getElementById('username').innerText = tg.initDataUnsafe.user?.first_name || 'Guest';
+const userName = tg.initDataUnsafe.user?.first_name || 'Guest';
 
-document.getElementById('closeBtn').addEventListener('click', () => {
+const welcomeMessage = document.getElementById('welcome-message');
+const usernameElement = document.getElementById('username');
+usernameElement.textContent = userName;
+
+let colors = ['#ff4081', '#ffa000', '#3f51b5', '#76ff03', '#1de9b6'];
+let i = 0;
+
+function changeUsernameColor() {
+    usernameElement.style.color = colors[i];
+    i = (i + 1) % colors.length;
+}
+
+setInterval(changeUsernameColor, 1000);
+
+document.getElementById('start-button').addEventListener('click', () => {
     tg.close();
 });
 
